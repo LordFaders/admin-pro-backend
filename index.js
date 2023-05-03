9ee8ffb8f6f58726532e88b307552d4531f1b5a7
Jaime Tapia <jaime.tapia.miquel@gmail.com>
2023-05-03 10:53:57 -0400

    SPA control

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api/upload', require('./routes/uploads'));
 // Lo último
 app.use(express.static(path.join(__dirname, '/public')));
 app.get('*', function(req, res) {
-    res.sendFile(__dirname + 'public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 
